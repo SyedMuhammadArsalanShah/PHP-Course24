bab7d0e3dfe146de585d3ced96d1de5177c4bae9
SyedMuhammadArsalanShah <smarsalanshah125@gmail.com>
2024-07-27 14:28:13 +0500

    code updated

--- Lec04ToDoWeb.php
+++ Lec04ToDoWeb.php
@@ -1,3 +1,4 @@
+```php
 <?php
 $server = "localhost";
 $username = "root";
@@ -6,22 +7,16 @@ $database = "aptechgls2";
 
 $con = mysqli_connect($server, $username, $password, $database);
 if (!$con) {
-
-
     die("connection is failed" . mysqli_connect_error());
 }
 
-
 if (isset($_GET["delete"])) {
-
     $id = $_GET["delete"];
-
-    $delquery = "delete from info where Id ='$id' ";
-
+    $delquery = "DELETE FROM info WHERE Id ='$id'";
     $resultdel = mysqli_query($con, $delquery);
 
     if ($resultdel) {
-        echo "deleted ";
+        echo "deleted";
         header("location:Lec04ToDoWeb.php");
     }
 }
@@ -29,49 +24,24 @@ if (isset($_GET["delete"])) {
 if (isset($_POST["email"])) {
     $email = $_POST["email"];
     $description = $_POST["desc"];
-
-
-
-    $sqliinsert = "INSERT INTO `info`( `Email`, `Description`) VALUES ('$email','$description')";
-
-
-
+    $sqliinsert = "INSERT INTO `info`(`Email`, `Description`) VALUES ('$email','$description')";
     $res = mysqli_query($con, $sqliinsert);
 
     if ($res) {
         echo "inserted";
- 
     }
 } else if (isset($_POST["emailupdate"])) {
-    # code...
-
-
-
     $id = $_POST["idupdate"];
     $emailaddress = $_POST["emailupdate"];
     $desc = $_POST["descupdate"];
-
-
-
-
     $update = "UPDATE `info` SET `Email`='$emailaddress',`Description`='$desc' WHERE Id = '$id'";
-
-
-
     $result = mysqli_query($con, $update);
 
     if ($result) {
         echo "updated";
     }
 }
-
-
-
-
-
 ?>
-
-
 
 <!doctype html>
 <html lang="en">
@@ -84,8 +54,6 @@ if (isset($_POST["email"])) {
 </head>
 
 <body>
-
-
     <!-- Button trigger modal -->
     <!-- <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
         Launch demo modal
@@ -96,7 +64,7 @@ if (isset($_POST["email"])) {
         <div class="modal-dialog">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h1 class="modal-title fs-5" id="exampleModalLabel">Update Your Records </h1>
+                    <h1 class="modal-title fs-5" id="exampleModalLabel">Update Your Records</h1>
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
                 <div class="modal-body">
@@ -110,24 +78,17 @@ if (isset($_POST["email"])) {
                             <label for="exampleFormControlTextarea1" class="form-label">Description</label>
                             <textarea class="form-control" name="descupdate" id="descV" rows="3"></textarea>
                         </div>
-
-
                         <div class="modal-footer">
                             <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
                             <button type="submit" class="btn btn-primary">Save changes</button>
                         </div>
-
-
                     </form>
                 </div>
-
             </div>
         </div>
     </div>
     <div class="container">
-
         <form action="Lec04ToDoWeb.php" method="post">
-
             <div class="mb-3">
                 <label for="exampleFormControlInput1" class="form-label">Email address</label>
                 <input type="email" name="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
@@ -136,17 +97,9 @@ if (isset($_POST["email"])) {
                 <label for="exampleFormControlTextarea1" class="form-label">Description</label>
                 <textarea class="form-control" name="desc" id="exampleFormControlTextarea1" rows="3"></textarea>
             </div>
-
-
-
             <button class="btn btn-primary" type="submit">Submit</button>
-
         </form>
     </div>
-
-
-
-
     <div class="container">
         <table class="table">
             <thead>
@@ -159,139 +112,55 @@ if (isset($_POST["email"])) {
                 </tr>
             </thead>
             <tbody>
-
                 <?php
-
-                $sql = "select *  from  info";
+                $sql = "SELECT * FROM info";
                 $result = mysqli_query($con, $sql);
-
-                //  $row = mysqli_fetch_assoc($result);
-
-                //  echo "my data ". var_dump($row)."<br>";
-
-
-                 $counter=1;
+                $counter = 1;
                 while ($a = mysqli_fetch_assoc($result)) {
-
-                    echo "  <tr>";
-                    echo "<th scope='row'>" .$counter++  . "</th>";
+                    echo "<tr>";
+                    echo "<th scope='row'>" . $counter++ . "</th>";
                     echo "<td scope='row'>" . $a["Email"] . "</td>";
                     echo "<td scope='row'>" . $a["Description"] . "</td>";
                     echo "<td scope='row'>" . $a["Date"] . "</td>";
                     echo "<td scope='row'>
-                    
-                    
-                    <div class='d-grid gap-2 d-md-block'>
-                     <button class='btn btn-primary edit' data-bs-toggle='modal' data-bs-target='#exampleModal' type='button' id=" . $a["Id"] . ">Edit</button>
-                     <button class='btn btn-danger  delete' type='button'id=" . $a["Id"] . ">Delete</button>
-                    </div>
-                    
-                    
+                        <div class='d-grid gap-2 d-md-block'>
+                            <button class='btn btn-primary edit' data-bs-toggle='modal' data-bs-target='#exampleModal' type='button' id=" . $a["Id"] . ">Edit</button>
+                            <button class='btn btn-danger delete' type='button' id=" . $a["Id"] . ">Delete</button>
+                        </div>
                     </td>";
-
-
-                    echo " </tr>";
+                    echo "</tr>";
                 }
-
-
                 ?>
-
-
-
             </tbody>
         </table>
     </div>
-
-
-
-
-
-
-
-
-
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-
-
-
-
-
     <script>
         editkaro = document.getElementsByClassName("edit");
         Array.from(editkaro).forEach((index) => {
-
             index.addEventListener("click", (i) => {
-
-                console.log("button work horha hai ya nahi ", )
-
-
                 tr = i.target.parentNode.parentNode.parentNode;
-
                 title = tr.getElementsByTagName("td")[0].innerText;
                 desc = tr.getElementsByTagName("td")[1].innerText;
-
-
                 console.log(title, desc);
-
                 emailV.value = title;
                 descV.value = desc
-
                 userid.value = i.target.id;
-
-
-                console.log(i.target.id)
-
+                console.log(i.target.id);
             });
-
-
         });
-
-
-
-
 
         deletekaro = document.getElementsByClassName("delete");
-
-
-
         Array.from(deletekaro).forEach((index) => {
-
             index.addEventListener("click", (i) => {
-
-                console.log("delete is working.....")
-
-
                 sno = i.target.id;
-
                 if (confirm("Kya app delete krna chaty hen ???")) {
-
-                    window.location = `/PhpClasses11f/Lec04ToDoWeb.php?delete=${sno}`
-
+                    window.location = `/PhpClasses11f/Lec04ToDoWeb.php?delete=${sno}`;
                 }
-
-
-
-
-
             });
-
-
-
-
         });
     </script>
-
-
-
-
-
-
-
-
-
-
-
-
 </body>
 
 </html>
+```
